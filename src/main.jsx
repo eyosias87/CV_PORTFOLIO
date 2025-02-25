28f696e005a2beed1afa95700dc0b430676f8357
Eyosias Mamo <eyosias.mamo@chasacademy.se>
2025-02-25 01:22:37 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,5 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import './components/Header'; // Import Header component
-import './components/Hem';
-import './components/OmMig';
-import './components/CV';
-import './components/Portfölj';
-import './components/KontaktaMig';
 import './index.css';
 import App from './App';
 
